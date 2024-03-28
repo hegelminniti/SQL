2c2225d50e799725dfcef65f4be5eec240186fe0
hegelminniti <hegelminniti@gmail.com>
2024-03-28 13:27:44 +0100

    Esercizio 2 - Alter table

--- books.sql
+++ books.sql
@@ -12,3 +12,11 @@ CREATE TABLE Books (
     rating DECIMAL,
     stock_count INT
 )
+
+/* You want to add more details about each book. Modify the "Books" table to include columns for the publisher and the number of pages. */
+
+ALTER TABLE Books
+ADD COLUMN publisher TEXT
+
+ALTER TABLE Books
+ADD COLUMN number_of_pages INT
